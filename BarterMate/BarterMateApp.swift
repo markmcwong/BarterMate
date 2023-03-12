2006ff50de90c410985cf3b22dc60a050e614999
markmcwong <markmcwong@yahoo.com>
2023-03-12 23:54:31 +0800

    Initial Commit

---
+++ BarterMate/BarterMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarterMateApp.swift
+//  BarterMate
+//
+//  Created by mark on 12/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct BarterMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarterMate/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BarterMate
+//
+//  Created by mark on 12/3/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
