0f2526954e9b15710216e83bdb888c84b1d19489
markmcwong <markmcwong@yahoo.com>
2023-04-16 22:57:23 +0800

    Update chat views

--- BarterMate/Views/Chat/ChatListItemView.swift
+++ BarterMate/Views/Chat/ChatListItemView.swift
@@ -25,59 +25,64 @@ struct ChatListItemView: ListItemView, View {
             item.fetchDetails()
         }
     }
-
+    
     var body: some View {
         Button(action: {
             GlobalState.shared.currentChat = item
             goToMessage = true
         }) {
-            HStack {
-                VStack(alignment: .leading, spacing: 10) {
-                    Text(item.name ?? "Default chat name")
-                        .font(.headline)
-                        .foregroundColor(.primary)
-                    HStack(spacing: 10) {
-                        if let users = item.users {
-                            ForEach(users, id: \.id) { user in
-                                Text(user.username)
-                                    .font(.subheadline)
-                                    .foregroundColor(.secondary)
-                                    .padding(5)
-                                    .background(Color.green.opacity(0.2))
-                                    .cornerRadius(10)
+            VStack(alignment: .leading, spacing: 10) {
+                HStack {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text(item.name ?? "Default chat name")
+                            .font(.headline)
+                            .foregroundColor(.primary)
+                        HStack(spacing: 10) {
+                            if let users = item.users {
+                                ForEach(users, id: \.id) { user in
+                                    Text(user.username)
+                                        .font(.subheadline)
+                                        .foregroundColor(.secondary)
+                                        .padding(5)
+                                        .background(Color.green.opacity(0.2))
+                                        .cornerRadius(10)
+                                }
                             }
                         }
+                    }
+                    Spacer()
+                    Button(action: {
+                        if model != nil {
+                            model!.deleteItem(item: item)
+                        } else {
+                            print("model does not exist for deleting item")
+                        }
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(.red)
+//                            .padding(10)
+                            .background(Color.white.opacity(0.9))
+                            .clipShape(Circle())
+//                            .shadow(color: .gray, radius: 3, x: 2, y: 2)
                     }
                 }
                 .padding()
                 .background(Color.white)
                 .cornerRadius(10)
                 .shadow(color: .gray, radius: 3, x: 2, y: 2)
-
-                Spacer()
-
-                Button(action: {
-                    if model != nil {
-                        model!.deleteItem(item: item)
-                    } else {
-                        print("model does not exist for deleting item")
-                    }
-                }) {
-                    Image(systemName: "xmark.circle.fill")
-                        .foregroundColor(.red)
-                        .padding(10)
-                        .background(Color.white.opacity(0.9))
-                        .clipShape(Circle())
-                        .shadow(color: .gray, radius: 3, x: 2, y: 2)
-                }
             }
             .padding(.horizontal)
         }
         .padding(5)
         .background(Color.clear)
-        .sheet(isPresented: $goToMessage) {
-            MessageView(viewModel: MessageViewModel(chat: GlobalState.shared.currentChat))
-        }
+        NavigationLink(
+           "",
+           destination: LazyView {
+               MessageView(viewModel: MessageViewModel(chat: GlobalState.shared.currentChat))
+           },
+           isActive: $goToMessage
+         )
+       .hidden()
     }
 }
 
